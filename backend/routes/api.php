9651c93f97dfc6575542d062e4d3d48078022409
TruongVanThuong <tvt230101@gmail.com>
2024-04-10 01:47:31 +0700

    manager account and update dashboard

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\ShopController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -37,45 +39,61 @@ Route::group([
 
 // middleware AdminAccessMDW ============
 // Route::middleware(['AdminAccessMDW'])->group(function () {
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+Route::post('refresh', 'AuthController@refresh');
+Route::post('me', 'AuthController@me');
 
-    Route::group(["prefix" => "/admin"], function () {
-        Route::group(["prefix" => "/product"], function () {
-            Route::post('/', [ProductController::class, 'index']);
-            Route::get('/show', [ProductController::class, 'prdShow']);
-            Route::get('/edit/{id}', [ProductController::class, 'prdEdit']);
-            Route::post('/update/{id}', [ProductController::class, 'prdUpdate']);
-            Route::delete('/delete/{id}', [ProductController::class, 'prdDelete']);
-        });
+Route::group(["prefix" => "/admin"], function () {
+    Route::group(["prefix" => "/product"], function () {
+        Route::post('/', [ProductController::class, 'index']);
+        Route::get('/show', [ProductController::class, 'prdShow']);
+        Route::get('/edit/{id}', [ProductController::class, 'prdEdit']);
+        Route::post('/update/{id}', [ProductController::class, 'prdUpdate']);
+        Route::delete('/delete/{id}', [ProductController::class, 'prdDelete']);
+    });
 
-        Route::controller(CategoryController::class)->group(function () {
-            Route::group(["prefix" => "/category"], function () {
-                Route::get('/', 'cateShow');
-                Route::post('/add', 'cateAdd');
-                Route::get('/edit/{id}', 'cateEdit');
-                Route::put('/update/{id}', 'cateUpdate');
-                Route::delete('/delete/{id}', 'cateDelete');
-            });
-        });
-
-        Route::controller(RoleController::class)->group(function () {
-            Route::group(["prefix" => "/role"], function () {
-                Route::get('/', 'roleShow');
-                Route::post('/add', 'roleAdd');
-                Route::get('/edit/{id}', 'roleEdit');
-                Route::put('/update/{id}', 'roleUpdate');
-                Route::delete('/delete/{id}', 'roleDelete');
-            });
-        });
-
-        Route::group(["prefix" => "/account"], function () {
-            Route::get('/', [AccountController::class, 'showAcc']);
-            Route::post('/add', [AccountController::class, 'addAcc']);
-            Route::get('/edit/{id}', [AccountController::class, 'editAcc']);
-            Route::post('/update/{id}', [AccountController::class, 'updateAcc']);
+    Route::controller(CategoryController::class)->group(function () {
+        Route::group(["prefix" => "/category"], function () {
+            Route::get('/', 'cateShow');
+            Route::post('/add', 'cateAdd');
+            Route::get('/edit/{id}', 'cateEdit');
+            Route::put('/update/{id}', 'cateUpdate');
+            Route::delete('/delete/{id}', 'cateDelete');
         });
     });
+
+    Route::controller(RoleController::class)->group(function () {
+        Route::group(["prefix" => "/role"], function () {
+            Route::get('/', 'roleShow');
+            Route::post('/add', 'roleAdd');
+            Route::get('/edit/{id}', 'roleEdit');
+            Route::put('/update/{id}', 'roleUpdate');
+            Route::delete('/delete/{id}', 'roleDelete');
+        });
+    });
+
+    Route::group(["prefix" => "/account"], function () {
+        Route::get('/', [AccountController::class, 'showAcc']);
+        Route::post('/add', [AccountController::class, 'addAcc']);
+        Route::get('/edit/{id}', [AccountController::class, 'editAcc']);
+        Route::post('/update/{id}', [AccountController::class, 'updateAcc']);
+
+        Route::controller(OrderController::class)->group(function () {
+            Route::group(["prefix" => "/order"], function () {
+                Route::get('/', 'orderShow');
+                Route::get('/edit/{id}', 'orderEdit');
+                Route::post('/update/{id}', 'orderUpdate');
+            });
+        });
+
+        Route::controller(TransactionController::class)->group(function () {
+            Route::group(["prefix" => "/transaction"], function () {
+                Route::get('/', 'tranShow');
+                Route::get('/edit/{id}', 'tranEdit');
+                Route::post('/update/{id}', 'tranUpdate');
+            });
+        });
+    });
+});
 // });
 // END middleware AdminAccessMDW ============
 
@@ -88,7 +106,10 @@ Route::group([
 Route::controller(ShopController::class)->group(function () {
     Route::group(["prefix" => "/shop"], function () {
         Route::get('/', 'shopShow');
+        Route::get('/checkout/{id}', 'checkoutShow');
+        Route::post('/checkout', 'checkout');
     });
+    Route::get('/order/{id}', 'getOrder');
 });
 
 Route::get('/dashboard/edit/{id}', [AccountController::class, 'dashEdit']);
--- dashboard/src/env.js
+++ dashboard/src/env.js
@@ -2,6 +2,7 @@ export default function AuthUser(){
   const endpointApi = "http://localhost:8001/api";
 
   const endpoint = "http://localhost:8001";
-
+ 
+  
   return { endpoint, endpointApi };
 }
